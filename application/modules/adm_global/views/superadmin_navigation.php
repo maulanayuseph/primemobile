62b1af34a6634e23893107a5efc372f37c81c0aa
FAJAR\Fajar Trio Wijanarko <fajar.trw@gmail.com>
2017-12-28 17:13:12 +0700

    list navigasi superadmin

--- application/modules/adm_global/views/superadmin_navigation.php
+++ application/modules/adm_global/views/superadmin_navigation.php
@@ -23,18 +23,103 @@
     <!-- sidebar menu -->
     <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
       <div class="menu_section">
-        <h3>General</h3>
+        <h3>Manajemen Prime Mobile</h3>
 
         <ul class="nav side-menu">
+
           <!-- MENU MASTER -->
           <li><a><i class="fa fa-archive" aria-hidden="true"></i> Master <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
               <li><a href="<?php echo base_url('adm_master/kelas');?>">Kelas</a></li>
               <li><a href="<?php echo base_url('adm_master/kurikulum');?>">Kode Kurikulum</a></li>
-              <li><a href="<?php echo base_url('adm_master/kur_kelas');?>">Kurikulum Kelas</a></li>
+              <li><a href="<?php echo base_url('adm_master/mapel');?>">Mata Pelajaran</a></li>
+              <li><a href="<?php echo base_url('adm_master/group');?>">Group</a></li>
+              <li><a href="<?php echo base_url('adm_master/indikator');?>">Indikator</a></li>
             </ul>
           </li>
-          <!-- MENU MIGRASI DB -->
+          <!-- MENU MASTER -->
+
+          <!-- MENU MANAJEMEN KONTEN -->
+          <li><a><i class="fa fa-folder" aria-hidden="true"></i> Manajemen Konten <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_konten/kur_kelas');?>">Kurikulum Kelas</a></li>
+              <li><a href="<?php echo base_url('adm_konten/kur_mapel');?>">Manajemen Mapel</a></li>
+              <li><a href="<?php echo base_url('adm_konten/kur_tema');?>">Manajemen Tematik</a></li>
+              <li><a href="<?php echo base_url('adm_pengujian/uji_eksternal');?>">Pengujian Eksternal</a></li>
+            </ul>
+          </li>
+          <!-- END MENU MANAJEMEN KONTEN -->
+          
+          <!-- MANAJEMEN BANK SOAL DAN CBT -->
+          <li><a><i class="fa fa-briefcase" aria-hidden="true"></i> Bank Soal & CBT <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_bank/kategori');?>">Kategori Bank Soal</a></li>
+              <li><a href="<?php echo base_url('adm_bank/index');?>">Bank Soal</a></li>
+              <li><a href="<?php echo base_url('adm_cbt/index');?>">CBT</a></li>
+              <li><a href="<?php echo base_url('adm_uss/index');?>">USS SBMPTN</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen bank soal dan cbt -->
+          
+          <!-- MANAJEMEN QUALITY CONTROL -->
+          <li><a><i class="fa fa-eye" aria-hidden="true"></i> Quality Control <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_qc/mapel');?>">Konten Mapel</a></li>
+              <li><a href="<?php echo base_url('adm_qc/tematik');?>">Konten Tematik</a></li>
+              <li><a href="<?php echo base_url('adm_cbt/bank_soal');?>">Bank Soal</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen quality control -->
+          
+          <!-- manajemen event -->
+          <li><a href="<?php echo base_url('adm_event/index');?>"><i class="fa fa-flag-checkered"></i> Event</a></li>
+          <!-- end manajemen event -->
+
+          <!-- manajemen pembelian paket -->
+          <li><a><i class="fa fa-money" aria-hidden="true"></i> Paket <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_paket/pembelian');?>">Pembelian</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen pembelian paket --> 
+
+          <!-- manajemen Self Assesment -->
+          <li><a><i class="fa fa-bar-chart" aria-hidden="true"></i> Pengembangan Diri <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_agcu/diagnostic');?>">AGCU</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen Self Assesment -->
+
+          <!-- MANAJEMEN VIDEO -->
+          <li><a><i class="fa fa-play" aria-hidden="true"></i> Video <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_video/group');?>">Group</a></li>
+              <li><a href="<?php echo base_url('adm_video/crew');?>">Crew</a></li>
+              <li><a href="<?php echo base_url('adm_video/index');?>">Video</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen video -->
+          
+          <!-- MANAJEMEN PSEP -->
+          <li><a><i class="fa fa-university" aria-hidden="true"></i> PSEP <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_psep/sekolah');?>">Sekolah</a></li>
+              <li><a href="<?php echo base_url('adm_psep/akun');?>">Akun Sekolah</a></li>
+              <li><a href="<?php echo base_url('adm_psep/import');?>">Siswa</a></li>
+              <li><a href="<?php echo base_url('adm_psep/commited');?>">PSEP Commited</a></li>
+            </ul>
+          </li>
+          <!-- end manajemen PSEP -->
+
+          <!-- manajemen admin -->
+          <li><a><i class="fa fa-user" aria-hidden="true"></i> Akun <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+              <li><a href="<?php echo base_url('adm_akun/akun');?>">Daftar Akun</a></li>
+              <li><a href="<?php echo base_url('adm_akun/tambah');?>">Tambah Baru</a></li>
+            </ul>
+          </li>
+          <!-- end MANAJEMEN ADMIN -->
 
           <!-- MENU MIGRASI DB -->
           <li><a><i class="fa fa-arrows-h" aria-hidden="true"></i> Migration <span class="fa fa-chevron-down"></span></a>
